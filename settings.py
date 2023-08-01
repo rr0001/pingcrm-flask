d491f4368948b2d58ae11db5f38834c62d2eeb22
Ralph R <10516663+rr0001@users.noreply.github.com>
2023-08-01 15:07:40 -0500

    fix: FileNotFoundError when missing .env and .secrets files

--- settings.py
+++ settings.py
@@ -1,14 +1,18 @@
 import os
 from collections import ChainMap
-from decouple import Config, RepositoryEnv
+from decouple import Config, RepositoryEnv, AutoConfig
 
 
 DOTENV_FILE = os.environ.get("DOTENV_FILE", ".env")
 SECRETS_FILE = os.environ.get("SECRETS_FILE", ".secrets")
 
-config = Config(
-    ChainMap(
-        RepositoryEnv(DOTENV_FILE),
-        RepositoryEnv(SECRETS_FILE),
+try:
+    config = Config(
+        ChainMap(
+            RepositoryEnv(DOTENV_FILE),
+            RepositoryEnv(SECRETS_FILE),
+        )
     )
-)
+except FileNotFoundError:
+    print("Modify environment variables using .env and .secrets files.")
+    config = AutoConfig()
